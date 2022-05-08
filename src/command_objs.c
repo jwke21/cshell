f42d3f27d6313cdbd41d3994e508e84e6b28c5f3
jwke21 <jakevm21@gmail.com>
2022-05-08 13:54:47 -0700

    Implemented instantiation and destruction functions for exit and game commands. Implemented make_cmds and destroy_cmds functions.

--- src/command_objs.c
+++ src/command_objs.c
@@ -15,7 +15,7 @@ char *desc_exit = "";
 /* Description of game() function. */
 char *desc_game = "";
 
-/********** INSTANTIATION OF COMMAND OBJECTS **********/
+/********** HELPER METHODS TO INSTANTIATE COMMAND OBJECTS **********/
 
 /* Instantiates cd Command object. */
 int make_cd(void) 
@@ -26,7 +26,6 @@ int make_cd(void)
 
   /* String reference. */
   char *name = "cd";
-  
   char *cd_name = malloc(sizeof(char) * strlen(name) + 1);
   strcpy(cd_name, name);
 
@@ -66,8 +65,7 @@ int make_help(void)
   if (cmd_help == NULL) return -1;
 
   /* String reference. */
-  char * name = "help";
-  
+  char *name = "help";
   char *help_name = malloc(sizeof(char) * strlen(help) + 1);
   strcpy(help_name, name);
 
@@ -83,14 +81,113 @@ int make_help(void)
   "    help\n"
   "  or"
   "    help <command>\n";
+  char *help_desc = malloc(sizeof(char) * strlen(text) + 1);
+
+  /* Assignment of values to Command object. */
+  cmd_help->name = &cd_name;
+  cmd_help->func = &seashell_help;
+  cmd_help->desc = &help_desc;
+
+  return 0;
 }
 
 /* Destroys help Command object. */
-int destroy_help(void)
+void destroy_help(void)
 {
   free(*cmd_help->name);
   free(*cmd_help->desc);
   free(cmd_help);
 }
 
+/* Makes exit Command object. */
+int make_exit(void)
+{
+  cmd_exit = malloc(sizeof(Command));
 
+  if (cmd_exit == NULL) return -1;
+
+  /* String reference. */
+  char *name = "exit";
+  char *exit_name = malloc(sizeof(char) * strlen(exit) + 1);
+  strcpy(exit_name, name);
+
+  /* Description of command to be used in help(). */
+  char *text =
+  "NAME\n"
+  "    exit\n\n"
+  "DESCRIPTION\n"
+  "    Closes the shell.\n\n"
+  "USAGE"
+  "    exit\n";
+  char *exit_desc = malloc(sizeof(char) * strlen(text) + 1);
+
+  /*Assignment of values to Command object. */
+  cmd_exit->name = &exit_name;
+  cmd_exit->func = &seashell_exit;
+  cmd_exit->desc = &exit_desc;
+
+  return 0;
+}
+
+/* Destroys exit command object. */
+int destroy_exit(void)
+{
+  free(*cmd_exit->name);
+  free(*cmd_exit->desc);
+  free(cmd_exit);
+}
+
+/* Makes game Command object */
+int make_game(void)
+{
+  cmd_game = malloc(sizeof(Command));
+
+  if (cmd_game == NULL) return -1;
+
+  /* String reference. */
+  char *name = "game";
+  char *game_name = malloc(sizeof(char) * strlen(name) + 1);
+  strcpy(game_name, name);
+
+  /* Description of command to be used in help(). */
+  char *text =
+  "NAME\n"
+  "    game\n\n"
+  "DESCRIPTION\n"
+  "    Runs a guessing game where the user must guess a number "
+  "between 1 and 10 (inclusive). The player gets three tries to "
+  "guess before failue.\n\n"
+  "USAGE\n"
+  "    game\n";
+  char *game_desc = malloc(sizeof(cahar) * strlen(text) + 1);
+
+  /* Assignment of values to Command object. */
+  cmd_game->name = &game_name;
+  cmd_game->func = &seashell_game;
+  cmd_game->desc = &game_desc;
+
+  return 0;
+}
+
+/********** FUNCTIONS TO INSTANTIATE COMMAND OBJECTS **********/
+int make_cmds(void)
+{
+  if (
+    make_cd() == -1 ||
+    make_help() == -1 ||
+    make_exit() == -1 ||
+    make_game() == -1 ||
+  ) {
+    return -1;
+  }
+  
+  return 0;
+}
+
+void destroy_cmds(void)
+{
+  destroy_cd();
+  destroy_help();
+  destroy_exit();
+  destroy_game();
+}
