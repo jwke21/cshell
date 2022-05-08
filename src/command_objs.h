196f90721ba46a665c279f674ad563ebbc3bb0c0
jwke21 <jakevm21@gmail.com>
2022-05-07 21:25:42 -0700

    Header file for instantation of Command objects.

--- src/command_objs.h
+++ src/command_objs.h
@@ -3,16 +3,19 @@
 #ifndef COMMAND_OBJS_H
 #define COMMAND_OBJS_H
 
+/* Define a function pointer for a command function type. */
+typedef int (*cmd_func) (char **);
+
 /* Command struct stores info about a built-in command. */
 typedef struct Command
 {
-  char *cmd_name;            // Pointer to string reference
-  int (*cmd_func) (char **); // Pointer to function
-  char **help_desc;          // Pointer to description of func
-}
+  char *name;     // Pointer to string reference
+  cmd_func func;  // Pointer to function
+  char *desc;     // Pointer to description of func
+} Command;
 
 /* Array of string references for built in commands. */
-char *command_names[NUM_COMMANDS] = {
+char *cmd_names[NUM_COMMANDS] = {
   "cd",
   "help",
   "exit",
@@ -20,11 +23,50 @@ char *command_names[NUM_COMMANDS] = {
 }
 
 /* Array of function pointers for built in commands. */
-int (*command_funcs[]) (char **) = {
+int (*cmd_funcs[NUM_COMMANDS]) (char **) = {
   &seashell_cd;
   &seashell_help;
   &seashell_exit;
   &seashell_game
 }
+
+/*
+ * NAME 
+ *    make_cmds
+ *
+ * DESCRIPTION
+ *    Instantiates a Command object for each of the built-in
+ *    shell commands which can be then referenced via the 
+ *    pointers declared below.
+ *
+ * RETURNS
+ *    0 if all Command objects were successfully instantiated.
+ *    -1 if there was a failure.
+*/
+int make_cmds(void);
+
+/*
+ * NAME
+ *    destory_cmds
+ *
+ * DESCRIPTION
+ *    Destroys all Command objects for each of the built-in
+ *    shell commands.
+ *
+ * RETURNS
+ *    0 if all the Command objects were successfully destroyed.
+ *    -1 if there was a failure.
+*/
+int destroy_cmds(void);
+
+/********** POINTERS TO COMMAND OBJECTS ************/
+
+Command *cmd_cd;
+
+Command *cmd_help;
+
+Command *cmd_exit;
+
+Command *cmd_game;
 
 #endif // COMMAND_OBJS_H
