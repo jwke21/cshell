dc18ef5380ac5f46af1324c86168da8b993f68e0
jwke21 <jakevm21@gmail.com>
2022-05-09 16:44:53 -0700

    Fixed some issues causing compiler warnings with -Wall flag.

--- src/commands.c
+++ src/commands.c
@@ -4,6 +4,15 @@
 #include <string.h>
 #include <time.h>
 
+#include "commands.h"
+
+/* Array of string references for built in commands. */
+char *cmd_names[NUM_COMMANDS] = {
+  "cd",
+  "help",
+  "exit",
+  "game"
+};
 
 /********** IMPLEMENTATION OF COMMAND FUNCTIONS **********/
 
@@ -33,7 +42,7 @@ int seashell_help(char **args)
     printf("The following commands are built in:\n");
     
     for (i = 0; i < NUM_COMMANDS; i++) {
-      printf("%d : %s\n", i + 1, commands[i]);
+      printf("%d : %s\n", i + 1, cmd_names[i]);
     }
     printf("\n");
 
@@ -108,3 +117,138 @@ int make_help(void)
   if (cmd_help == NULL) return -1;
 
   /* String reference. */
+  char *name = "help";
+  char *help_name = malloc(sizeof(char) * strlen(name) + 1);
+  strcpy(help_name, name);
+
+  /* Description of command to be used in help(). */
+  char *text =
+  "NAME\n"
+  "    help\n\n"
+  "DESCRIPTION\n"
+  "    Prints out introduction to shell and the commands that are "
+  "built into it. If built-in command name is provided, will print "
+  "out a description of that command.\n\n"
+  "USAGE\n"
+  "    help\n"
+  "  or"
+  "    help <command>\n";
+  char *help_desc = malloc(sizeof(char) * strlen(text) + 1);
+
+  /* Assignment of values to Command object. */
+  cmd_help->name = &help_name;
+  cmd_help->func = &seashell_help;
+  cmd_help->desc = &help_desc;
+
+  return 0;
+}
+
+/* Destroys help Command object. */
+void destroy_help(void)
+{
+  free(*cmd_help->name);
+  free(*cmd_help->desc);
+  free(cmd_help);
+}
+
+/* Makes exit Command object. */
+int make_exit(void)
+{
+  cmd_exit = malloc(sizeof(Command));
+
+  if (cmd_exit == NULL) return -1;
+
+  /* String reference. */
+  char *name = "exit";
+  char *exit_name = malloc(sizeof(char) * strlen(name) + 1);
+  strcpy(exit_name, name);
+
+  /* Description of command to be used in help(). */
+  char *text =
+  "NAME\n"
+  "    exit\n\n"
+  "DESCRIPTION\n"
+  "    Closes the shell.\n\n"
+  "USAGE"
+  "    exit\n";
+  char *exit_desc = malloc(sizeof(char) * strlen(text) + 1);
+
+  /*Assignment of values to Command object. */
+  cmd_exit->name = &exit_name;
+  cmd_exit->func = &seashell_exit;
+  cmd_exit->desc = &exit_desc;
+
+  return 0;
+}
+
+/* Destroys exit command object. */
+void destroy_exit(void)
+{
+  free(*cmd_exit->name);
+  free(*cmd_exit->desc);
+  free(cmd_exit);
+}
+
+/* Makes game Command object */
+int make_game(void)
+{
+  cmd_game = malloc(sizeof(Command));
+
+  if (cmd_game == NULL) return -1;
+
+  /* String reference. */
+  char *name = "game";
+  char *game_name = malloc(sizeof(char) * strlen(name) + 1);
+  strcpy(game_name, name);
+
+  /* Description of command to be used in help(). */
+  char *text =
+  "NAME\n"
+  "    game\n\n"
+  "DESCRIPTION\n"
+  "    Runs a guessing game where the user must guess a number "
+  "between 1 and 10 (inclusive). The player gets three tries to "
+  "guess before failue.\n\n"
+  "USAGE\n"
+  "    game\n";
+  char *game_desc = malloc(sizeof(char) * strlen(text) + 1);
+
+  /* Assignment of values to Command object. */
+  cmd_game->name = &game_name;
+  cmd_game->func = &seashell_game;
+  cmd_game->desc = &game_desc;
+
+  return 0;
+}
+
+void destroy_game(void)
+{
+	free(*cmd_game->name);
+	free(*cmd_game->desc);
+	free(cmd_game);
+}
+
+/********** FUNCTIONS TO INSTANTIATE COMMAND OBJECTS **********/
+int make_cmds(void)
+{
+  if (
+    make_cd() == -1 ||
+    make_help() == -1 ||
+    make_exit() == -1 ||
+    make_game() == -1
+     ) {
+    return -1;
+  }
+  
+  return 0;
+}
+
+void destroy_cmds(void)
+{
+  destroy_cd();
+  destroy_help();
+  destroy_exit();
+  destroy_game();
+}
+
+
